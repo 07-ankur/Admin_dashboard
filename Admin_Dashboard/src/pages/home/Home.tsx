1c8073c7ed5a4389f63db42d23ab9272ee69be59
07-ankur <im4ankur@gmail.com>
2023-11-06 18:00:58 +0530

    Added Bar Chart

--- Admin_Dashboard/src/pages/home/Home.tsx
+++ Admin_Dashboard/src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import BarChartBox from "../../components/BarChartBox/barChartBox";
+import { barChartBoxRevenue, barChartBoxVisit } from "../../data";
 import ChartBox from "../../components/ChartBox/ChartBox"
 import TopBox from "../../components/TopBox/TopBox"
 import {
@@ -20,8 +22,8 @@ const Home = () => {
       <div className="box box5"><ChartBox {...chartBoxConversion} /></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue} /></div>
       <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
+      <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div>
   )
 }
